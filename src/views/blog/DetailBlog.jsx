3e94a7afa5be71d94dca3ffd7bebaaf514d67e30
iamVuong <iam.hungvuong21@gmail.com>
2021-03-15 14:28:32 +0700

    banner

--- src/views/blog/DetailBlog.jsx
+++ src/views/blog/DetailBlog.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import BaseLayout from "../../components/BaseLayout/BaseLayout1";
 import axios from "../../config/axios/axios"
 import "../blog/DetailBlog.css"
-import { Col, Row, Image, Input, BackTop, Affix, Avatar } from 'antd';
+import { Col, Row, Image, Input, BackTop, Affix, Avatar , Button} from 'antd';
 import {
     createFromIconfontCN, LeftCircleOutlined, ArrowUpOutlined
 } from '@ant-design/icons';
@@ -65,45 +65,47 @@ function DetailBlog(props) {
 
     return (
         <BaseLayout>
-            <div style={{ paddingBottom: "20px" }}>
-                <Row>
-                    <Col >
-                        <Affix offsetTop={240} onChange={affixed => console.log(affixed)}>
-                            <div className="icon-fix">
-                                <div>  <LeftCircleOutlined onClick={() => {
-                                    history.push("/blog")
-                                }} />  </div>
-                                <div> <IconFont type="icon-facebook" /> </div>
-                                <div> <IconFont type="icon-twitter" /> </div>
-                            </div>
+          <Affix offsetTop={240} onChange={affixed => console.log(affixed)}>
+                    <div className="icon-fix">
+                        <div>  <LeftCircleOutlined onClick={() => {
+                            history.push("/blog")
+                        }} />  
+                        </div>
+                        <div> <IconFont type="icon-facebook" /> </div>
+                        <div> <IconFont type="icon-twitter" /> </div>
+                    </div>
 
-                        </Affix >
+            </Affix >
+            <div className='container' style={{ paddingBottom: "20px" }}>
+              
+                <Row>
+                    <Col md={18}>
                         {<div>
-                            <div className="container" style={{ padding: "0 25px", marginBottom: "40px" }}>
+                            <div  style={{ padding: "0 25px", marginBottom: "70px" }}>
                                 <h1 style={{ paddingBottom: "20px", marginBottom: "30px", fontSize: "25px" }}> {dataBlog.title}  </h1>
                                 <div style={{ textAlign: "center", paddingBottom: "20px" }}>
-                                    <Image style={{ width: "500px", height: '300px' }}
+                                    <Image style={{ width: "100%", height: '400px' }}
                                         src={dataBlog.image}
                                     >
                                     </Image>
                                     <p style={{ textAlign: "center", marginTop: "15px" }}>{dataBlog.introduceImg}</p>
 
                                 </div>
-                                <h3> {dataBlog.content}   </h3>
+                                <p style={{fontSize: '18px', fontWeight: '400'}}> {dataBlog.content}   </p>
                             </div>
                         </div>
                         }
                         < hr style={{ height: "3px" }} />
                         <div style={{ paddingBottom: "30px" }}>
-                            <div className="container">
-                                <h3 style={{ fontSize: "20px", paddingBottom: "20px" }}> {comments.length} Comments </h3>
+                            <div>
+                                <h3 style={{fontSize: '20px' ,fontWeight: 'bolder', color:'#f69631', }}> {comments.length} COMMENTS </h3>
                                 <div className="comment-detailblog">
                                     {user.avatar ? user.avatar.length === 1 ? <Avatar>{user.avatar.toUpperCase()}</Avatar> : <Avatar src={user.avatar}></Avatar> : null}
                                     <Input placeholder="Y kien cua ban... " type="text" onChange={(e) => {
                                         setContentComment(e.target.value)
                                     }} />
                                 </div>
-                                <button type="button" style={{ marginTop: "10px", marginBottom: "20px", padding: "3px", cursor: "pointer" }} onClick={() => {
+                                <Button style={{marginBottom: '10px'}} type="primary" onClick={() => {
                                     axios({
                                         method: "POST",
                                         url: "/comment",
@@ -120,7 +122,7 @@ function DetailBlog(props) {
                                         .catch((err) => {
                                             alert(err.response.data.message)
                                         })
-                                }}> gui binh luan </button>
+                                }}> Gửi bình luận </Button>
                                 <div>
 
                                     {comments.length ? comments.map((item, index) => {
@@ -157,6 +159,30 @@ function DetailBlog(props) {
                                 </div>
                             </div>
 
+                        </div>
+                    </Col>
+                    <Col span={6}>
+                        <iframe style={{width: '100%',height:'600px', marginBottom:'20px', border: 'none'}} src="https://ds.polyad.net/ads/h/havenpark/2021/03/11/50635/300x600/dfp/pc/std/fixed/?link=https%3A%2F%2Fadclick.g.doubleclick.net%2Fpcs%2Fclick%3Fxai%3DAKAOjsvsTiaJPkBzDpZXAhbHMGAk2cfUupf7I5haxGGpJd9xIL0i7g8V-DFoHmhSHo52vK_vD5t92t1HH_hBx0xvfGzRRTWalvey212MuS_tVQwU8mC1xMj3qHaMIpPiONuZFEpQyb62Yg5y0xZH51jhE7kTadjetcGgfVJTlVftf-Fut9lXsr7zMbhbUwA0bUTBVBcuA-Hgwa6mucD6O1iIeB4Fw1bWMSZDBc9_zL_AH-sZqWJ3L7u3Tl4Q9aAXc4sVHJIU7GF6NQwz5eHJ5eKWjsLsMXQQ4KA-a1Uwf-ZjoW0_m1AI8quDLj6pcS9OEkTaVFKbZWXVbxYAG9jUcSs%26sai%3DAMfl-YTkW8b_BBv-yllLQ8NBCRpzKoNRDtVZNNyrnpMjXZsQLSK6IJUMe-XPjKlBouwC-EhAFuXHOSYkbIzbRWIsS2Wc4UL-H_2pyHlUSUEJgcvKhWRcqG9UpX42r4Fv6M8cykeU%26sig%3DCg0ArKJSzLpJOQP-9zBWEAE%26urlfix%3D1%26adurl%3Dhttps%253A%252F%252Fhavenpark.ecopark.com.vn%252F%253Futm_source%253DVNE%2526utm_medium%253Dlarge&otherlink=&campaign_name=HavenPark%20&index_brand=LB1001.8_Ecopark%20&index_industrial=L1001_B%E1%BA%A5t%20%C4%91%E1%BB%99ng%20s%E1%BA%A3n" alt=""/>
+                        <h3 style={{fontSize: '20px' ,fontWeight: 'bolder', color:'#f69631', }}> TIN THẾ GIỚI </h3>
+                        <div>
+                            <Image src='https://i1-vnexpress.vnecdn.net/2021/03/14/lloyd-austin-5-3549-1615710645.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=L198TOHoewn8imzQkvwASA'/>
+                            <div style={{width:'100%',height:'350px',overflowY: 'scroll', paddingBottom: '8px'}}>
+                                <div className='text-title' style={{paddingTop:'5px'}}>
+                                    <h3>Mỹ muốn cùng đồng minh châu Á răn đe Trung Quốc</h3>
+                                    <p>Bộ trưởng Quốc phòng Mỹ đề cao hợp tác quân sự với các đồng minh châu Á nhằm duy trì lợi thế, xây dựng khả năng răn đe Trung Quốc. </p>
+                                    <hr/>
+                                </div>
+                                <div  className='text-title' style={{paddingTop:'5px'}}>
+                                    <h3>Mỹ muốn cùng đồng minh châu Á răn đe Trung Quốc</h3>
+                                    <p>Bộ trưởng Quốc phòng Mỹ đề cao hợp tác quân sự với các đồng minh châu Á nhằm duy trì lợi thế, xây dựng khả năng răn đe Trung Quốc. </p>
+                                    <hr/>
+                                </div>
+                                <div  className='text-title' style={{paddingTop:'5px'}}>
+                                    <h3>Mỹ muốn cùng đồng minh châu Á răn đe Trung Quốc</h3>
+                                    <p>Bộ trưởng Quốc phòng Mỹ đề cao hợp tác quân sự với các đồng minh châu Á nhằm duy trì lợi thế, xây dựng khả năng răn đe Trung Quốc. </p>
+                                    <hr/>
+                                </div>
+                            </div>
                         </div>
                     </Col>
 
